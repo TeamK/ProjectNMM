6e5c98675ed7ede760e7ed625dbb6dc324bf6dde
321polorex123 <kevin.hinnen@hotmail.com>
2014-03-20 21:39:30 +0100

    New Project

---
+++ ProjectNMM.Model/ModelControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectNMM.Model
+{
+    public class ModelControl
+    {
+        public ModelControl()
+        { }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,12 @@
+License
+=======
+
+Code:	see license.txt
+Icon:	GPL / CC BY 3.0
+	http://www.gnu.org/licenses/gpl.html
+	http://creativecommons.org/licenses/by/3.0/
+
+Source
+======
+
+Icons:	http://icomoon.io/
