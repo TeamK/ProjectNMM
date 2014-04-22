41b9b98d2b0197c1a9b516b66eeff415c542ba9e
321polorex123 <kevin.hinnen@hotmail.com>
2014-04-22 20:44:58 +0200

    Typdefs

---
+++ ProjectNMM.Model/Typedefs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectNMM.Model
+{
+    public enum GameType
+    {
+        Undefined           = 0,
+        PlayerVsPlayer      = 1,
+        PlayerVsMachine     = 2,
+        MachineVsMachine    = 3
+    };
+}
